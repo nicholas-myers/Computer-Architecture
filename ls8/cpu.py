12612be91235867d20492dc62e13a503f9e63f18
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-08-20 12:17:18 -0700

    added the call and ret functionality to ls8

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,8 +10,10 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.reg = [0] * 8
+        self.reg[7] = 0xf4
         self.ram = [0] * 256
         self.pc = 0
+        self.stack = []
 
     def load(self):
         """Load a program into memory."""
@@ -19,7 +21,6 @@ class CPU:
         # 
         address = 0
 
-        # For now, we've just hardcoded a program:
         if len(sys.argv) != 2:
             print(sys.argv)
             print("usage: ls8.py examples/filename")
@@ -57,13 +58,15 @@ class CPU:
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
+        # print(reg_a, reg_b)
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
-        else:
-            raise Exception("Unsupported ALU operation")
-
+        if op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        # else:
+        #     raise Exception("Unsupported ALU operation")
+    
     def ram_read(self, MAR):
         # print(self.ram[MAR])
         return self.ram[MAR]
@@ -71,53 +74,87 @@ class CPU:
     def ram_write(self, MAR, MDR):
         self.ram[MAR] = MDR
         
-    def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
+    # def trace(self):
+    #     """
+    #     Handy function to print out the CPU state. You might want to call this
+    #     from run() if you need help debugging.
+    #     """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
+    #     print(f"TRACE: %02X | %02X %02X %02X |" % (
+    #         self.self.pc,
+    #         #self.fl,
+    #         #self.ie,
+    #         self.ram_read(self.pc),
+    #         self.ram_read(self.pc + 1),
+    #         self.ram_read(self.pc + 2)
+    #     ), end='')
 
-        for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+    #     for i in range(8):
+    #         print(" %02X" % self.reg[i], end='')
 
-        print()
+    #     print()
 
     def run(self):
         """Run the CPU."""
         # self.trace()
         # start running
         running = True
-        
         LDI = 0b10000010
         PRN = 0b01000111
         HLT = 0b00000001
-        MULTI = 0b10100010
+        MUL = 0b10100010
+        PUSH = 0b01000101
+        POP = 0b01000110
+        CALL = 0b01010000
+        ADD = 0b10100000
+        RET = 0b00010001
+        
         while running:
             # get the instruction from ram
             
             ir = self.ram_read(self.pc)
-            arg1 = self.pc + 1
-            arg2 = self.pc + 2
+            arg1 = self.ram_read(self.pc + 1)
+            arg2 = self.ram_read(self.pc + 2)
             # print(ir)
             if ir == LDI:
-                self.reg[self.ram_read(arg1)] = self.ram_read(arg2)
+                self.reg[arg1] = arg2
                 self.pc += 3
             if ir == PRN:
-                print(self.reg[self.ram_read(arg1)])
+                print(self.reg[arg1])
                 self.pc += 2
             if ir == HLT:
                 running = False
-            if ir == MULTI:
-                multiply = self.reg[self.ram_read(arg1)] * self.reg[self.ram_read(arg2)]
-                print(multiply)
+            if ir == ADD:
+                self.alu("ADD", arg1, arg2)
+                self.pc+=3
+            if ir == MUL:
+                self.alu("MUL", arg1, arg2)
+                # print(multiply)
                 self.pc += 3
-                return multiply
+            if ir == PUSH:
+                self.reg[7] -= 1
+                stack_top = self.reg[7]
+                self.ram[stack_top] = self.reg[arg1]
+                self.pc += 2
+                # print(f"stack: {self.ram[0xE4:0xF4]}")
+            if ir == POP:
+                stack_top = self.reg[7]
+                self.reg[arg1] = self.ram[stack_top]
+                self.reg[7] += 1
+                # print(f"stack: {self.ram[0xE4:0xF4]}")
+                self.pc += 2
+            if ir == CALL:
+                # print("this happened")
+                # print(self.pc)
+                ret_addr = self.pc + 2
+                self.reg[7] -= 1
+                self.ram[self.reg[7]] = ret_addr
+                # print(self.ram[self.reg[7]])
+                self.pc = self.reg[arg1]
+            if ir == RET:
+                # print("this happened")
+                ret_addr = self.ram[self.reg[7]]
+                # print(ret_addr)
+                self.reg[7] += 1
+                self.pc = ret_addr                
+                
