514b1549afe4fd3baa18d6ee7a78a7421b8cb04b
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-08-21 11:29:07 -0700

    optimized codes by putting helper functions related to each op, then checking the amount of arguments required for the op

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,31 +2,6 @@
 
 import sys
 
-ops = {
-    0b10100000: "ADD",
-    0b10101000: "AND",
-    0b01010000: "CALL",
-    0b10100111: "CMP",
-    0b00000001: "HLT",
-    0b01010101: "JEQ",
-    0b01010100: "JMP",
-    0b01010110: "JNE",
-    0b10000010: "LDI",
-    0b10100010: "MUL",
-    0b01000110: "POP",
-    0b01001000: "PRA",
-    0b01000111: "PRN",
-    0b01000101: "PUSH",
-    0b00010001: "RET",
-    0b10000100: "ST"
-}
-
-alus = {
-    0b10100000: "ADD",
-    0b10101000: "AND",
-    0b10100010: "MUL"
-}
-
 # Flag
 FL = [0] * 3
 # Stack Pointer
@@ -46,6 +21,41 @@ class CPU:
         self.ram = [0] * 256
         self.pc = 0
         self.running = False
+        self.ops = {
+            0b10100000: self.add_alu,
+            0b10101000: self.and_alu,
+            0b01010000: self.call,
+            0b10100111: self.cmp_alu,
+            0b00000001: self.hlt,                0b01010101: self.jeq,
+            0b01010100: self.jump,
+            0b01010110: self.jne,
+            0b10000010: self.ldi,
+            0b10100010: self.mul_alu,
+            0b01000110: self.pop_reg,
+            0b01001000: self.pra,
+            0b01000111: self.prn,
+            0b01000101: self.push_reg,
+            0b00010001: self.ret_sub,
+            0b10000100: self.st
+        }
+        self.args = {
+            0b10100000: 2,
+            0b10101000: 2,
+            0b01010000: 1,
+            0b10100111: 2,
+            0b00000001: 0,                
+            0b01010101: 1,
+            0b01010100: 1,
+            0b01010110: 1,
+            0b10000010: 2,
+            0b10100010: 2,
+            0b01000110: 1,
+            0b01001000: 1,
+            0b01000111: 1,
+            0b01000101: 1,
+            0b00010001: 0,
+            0b10000100: 2
+        }
         
     def load(self):
         """Load a program into memory."""
@@ -159,7 +169,7 @@ class CPU:
         self.reg[SP] -= 1
         self.ram[self.reg[SP]] = ret_addr
         # print(self.ram[self.reg[SP]])
-        self.pc = self.reg[arg1]    
+        self.pc = self.reg[reg_num]    
     
     def cmp_alu(self, reg_a, reg_b):
         if self.reg[reg_a] == self.reg[reg_b]:
@@ -207,6 +217,7 @@ class CPU:
 
     def mul_alu(self, reg_a, reg_b):
         self.reg[reg_a] *= self.reg[reg_b]
+        self.pc += 3
 
     def pra(self, reg_num):
         print(self.reg[reg_num])
@@ -236,130 +247,23 @@ class CPU:
         self.reg[SP] += 1
         self.pc = ret_addr
     
-    
+    def st(self, reg_a, reg_b):
+        addr = self.reg[reg_a]
+        value = self.reg[reg_b]
+        self.ram_write(addr, value)
+        self.pc += 3
     
     def run(self):
-        """Run the CPU."""
-        # self.trace()
-        # start running
         self.running = True
-        
-        LDI = 0b10000010
-        PRN = 0b01000111
-        HLT = 0b00000001
-        MUL = 0b10100010
-        PUSH = 0b01000101
-        POP = 0b01000110
-        CALL = 0b01010000
-        ADD = 0b10100000
-        RET = 0b00010001
-        JMP = 0b01010100
-        CMP = 0b10100111
-        JEQ = 0b01010101
-        JNE = 0b01010110
-        ST = 0b10000100
-        PRA = 0b01001000
-        AND = 0b10101000
-        
+
         while self.running:
-            # get the instruction from ram
-            ir = self.ram_read(self.pc)
+            IR = self.ram_read(self.pc)
             arg1 = self.ram_read(self.pc + 1)
             arg2 = self.ram_read(self.pc + 2)
-            # print(ir)
-            if ir == LDI:
-                self.reg[arg1] = arg2
-                self.pc += 3
-            if ir == PRN:
-                print(self.reg[arg1])
-                self.pc += 2
-            if ir == HLT:
-                self.running = False
-            if ir == ADD:
-                self.alu("ADD", arg1, arg2)
-                self.pc+=3
-            if ir == MUL:
-                self.alu("MUL", arg1, arg2)
-                # print(multiply)
-                self.pc += 3
-            if ir == PUSH:
-                self.reg[SP] -= 1
-                stack_top = self.reg[SP]
-                self.ram[stack_top] = self.reg[arg1]
-                self.pc += 2
-                # print(f"stack: {self.ram[0xE4:0xF4]}")
-            if ir == POP:
-                stack_top = self.reg[SP]
-                self.reg[arg1] = self.ram[stack_top]
-                self.reg[SP] += 1
-                # print(f"stack: {self.ram[0xE4:0xF4]}")
-                self.pc += 2
-            if ir == CALL:
-                # print("this happened")
-                # print(self.pc)
-                ret_addr = self.pc + 2
-                self.reg[SP] -= 1
-                self.ram[self.reg[SP]] = ret_addr
-                # print(self.ram[self.reg[SP]])
-                self.pc = self.reg[arg1]
-            if ir == RET:
-                # print("this happened")
-                ret_addr = self.ram[self.reg[SP]]
-                # print(ret_addr)
-                self.reg[SP] += 1
-                self.pc = ret_addr
-            if ir == JMP:
-                self.pc = self.reg[arg1]          
-            if ir == CMP:
-                self.alu("CMP", arg1, arg2)
-                self.pc += 3
-            if ir == JNE:
-                if int(FL[-1]) == 0:
-                    self.pc = self.reg[arg1]
-                else:
-                    self.pc += 2
-            if ir == JEQ:
-                if int(FL[-1]) == 1:
-                    self.pc = self.reg[arg1]
-                else:
-                    self.pc += 2
-            if ir == ST:
-                addr = self.reg[arg1]
-                value = self.reg[arg2]
-                self.ram_write(addr, value)
-                self.pc += 3
-            if ir == PRA:
-                print(self.reg[arg1])
-                self.pc += 2
-            if ir == AND:
-                self.alu("AND", arg1, arg2)
-                self.pc += 3
-    
-    # def run(self):
-    #     self.running = True
-
-    #     while self.running:
-    #         IR = self.ram_read(self.pc)
-    #         arg1 = self.ram_read(self.pc + 1)
-    #         arg2 = self.ram_read(self.pc + 2)
-            
-    #         ops = {
-    #             0b10100000: self.add_alu(arg1, arg2),
-    #             0b10101000: self.and_alu(arg1, arg2),
-    #             0b01010000: self.call(arg1),
-    #             0b10100111: self.cmp_alu(arg1, arg2),
-    #             0b00000001: self.hlt(),
-    #             0b01010101: self.jeq(arg1),
-    #             0b01010100: self.jump(arg1),
-    #             0b01010110: self.jne(arg1),
-    #             0b10000010: self.ldi(arg1, arg2),
-    #             0b10100010: self.mul_alu(arg1, arg2),
-    #             0b01000110: self.pop(arg1),
-    #             0b01001000: self.pra(arg1),
-    #             0b01000111: self.prn(arg1),
-    #             0b01000101: self.push(arg1),
-    #             0b00010001: self.ret_sub(),
-    #             0b10000100: "ST"
-    #         }
-            
-    #         ops[IR]
+        
+            if self.args[IR] == 2:
+                self.ops[IR](arg1, arg2)
+            if self.args[IR] == 1:
+                self.ops[IR](arg1)
+            if self.args[IR] == 0:
+                self.ops[IR]()
