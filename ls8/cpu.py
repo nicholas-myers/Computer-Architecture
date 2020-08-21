00f08dada94556e07abe8276b35586816593ff76
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-08-21 10:41:45 -0700

    still optimizing code

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -84,33 +84,6 @@ class CPU:
         if address == 0:
             print("program was Empty!")
 
-    
-    
-    def cmp_alu(self, reg_a, reg_b):
-        if self.reg[reg_a] == self.reg[reg_b]:
-            # set FL from E to 1
-            FL[-1] = 1
-        else:
-            # other wise set to 0
-            FL[-1] = 0
-            
-        if self.reg[reg_a] > self.reg[reg_b]:
-                # set FL from G to 1
-                FL[-2] = 1
-        else:
-            # otherwise to 0
-            FL[-2] = 0
-                            
-        if self.reg[reg_a] < self.reg[reg_b]:
-            # set FL bit from L to 1
-            FL[-3] = 1
-        else:
-            # otherwise set to 0
-            FL[-3] = 0
-    
-    def mul_alu(self, reg_a, reg_b):
-        self.reg[reg_a] *= self.reg[reg_b]
-        
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
         # print(reg_a, reg_b)
@@ -154,6 +127,7 @@ class CPU:
             self.reg[reg_a] *= self.reg[reg_b]
         # else:
         #     raise Exception("Unsupported ALU operation")
+    
     # ram functions
     def ram_read(self, MAR):
         # print(self.ram[MAR])
@@ -165,6 +139,7 @@ class CPU:
     # OP functions
     def add_alu(self, reg_a, reg_b):
         self.reg[reg_a] += self.reg[reg_b]
+        self.pc += 3
     
     def and_alu(self, reg_a, reg_b):
         num1 = int(self.reg[reg_a])
@@ -175,23 +150,93 @@ class CPU:
         print(bin(num2))
         res = bin(num1 & num2)
         print(res)
-        
+        self.pc += 3
+    
+    def call(self, reg_num):
+        # print("this happened")
+        # print(self.pc)
+        ret_addr = self.pc + 2
+        self.reg[SP] -= 1
+        self.ram[self.reg[SP]] = ret_addr
+        # print(self.ram[self.reg[SP]])
+        self.pc = self.reg[arg1]    
+    
+    def cmp_alu(self, reg_a, reg_b):
+        if self.reg[reg_a] == self.reg[reg_b]:
+            # set FL from E to 1
+            FL[-1] = 1
+        else:
+            # other wise set to 0
+            FL[-1] = 0
+            
+        if self.reg[reg_a] > self.reg[reg_b]:
+                # set FL from G to 1
+                FL[-2] = 1
+        else:
+            # otherwise to 0
+            FL[-2] = 0
+                            
+        if self.reg[reg_a] < self.reg[reg_b]:
+            # set FL bit from L to 1
+            FL[-3] = 1
+        else:
+            # otherwise set to 0
+            FL[-3] = 0
+        self.pc += 3
+    
+    def hlt(self):
+        self.running = False
+    
+    def jeq(self, reg_num):
+        if int(FL[-1]) == 1:
+            self.pc = self.reg[reg_num]
+        else:
+            self.pc += 2
+    
+    def jump(self, reg_num):
+        self.pc = self.reg[reg_num] 
+    
+    def jne(self, reg_num):
+        if int(FL[-1]) == 0:
+            self.pc = self.reg[reg_num]
+        else:
+            self.pc += 2
     def ldi(self, reg_num, value):
         self.reg[reg_num] = value
         self.pc += 3
 
-    def hlt(self):
-        self.running = False
+    def mul_alu(self, reg_a, reg_b):
+        self.reg[reg_a] *= self.reg[reg_b]
 
+    def pra(self, reg_num):
+        print(self.reg[reg_num])
+        self.pc += 2
+    
     def prn(self, reg_num):
         print(self.reg[reg_num])
         self.pc += 2
         
-    def push_reg(self):
-        pass
+    def push_reg(self, reg_num):
+        self.reg[SP] -= 1
+        stack_top = self.reg[SP]
+        self.ram[stack_top] = self.reg[reg_num]
+        self.pc += 2
     
-    def pop_reg(self):
-        pass
+    def pop_reg(self, reg_num):
+        stack_top = self.reg[SP]
+        self.reg[reg_num] = self.ram[stack_top]
+        self.reg[SP] += 1
+        # print(f"stack: {self.ram[0xE4:0xF4]}")
+        self.pc += 2
+    
+    def ret_sub(self):
+        # print("this happened")
+        ret_addr = self.ram[self.reg[SP]]
+        # print(ret_addr)
+        self.reg[SP] += 1
+        self.pc = ret_addr
+    
+    
     
     def run(self):
         """Run the CPU."""
@@ -218,7 +263,6 @@ class CPU:
         
         while self.running:
             # get the instruction from ram
-            
             ir = self.ram_read(self.pc)
             arg1 = self.ram_read(self.pc + 1)
             arg2 = self.ram_read(self.pc + 2)
@@ -290,3 +334,32 @@ class CPU:
             if ir == AND:
                 self.alu("AND", arg1, arg2)
                 self.pc += 3
+    
+    # def run(self):
+    #     self.running = True
+
+    #     while self.running:
+    #         IR = self.ram_read(self.pc)
+    #         arg1 = self.ram_read(self.pc + 1)
+    #         arg2 = self.ram_read(self.pc + 2)
+            
+    #         ops = {
+    #             0b10100000: self.add_alu(arg1, arg2),
+    #             0b10101000: self.and_alu(arg1, arg2),
+    #             0b01010000: self.call(arg1),
+    #             0b10100111: self.cmp_alu(arg1, arg2),
+    #             0b00000001: self.hlt(),
+    #             0b01010101: self.jeq(arg1),
+    #             0b01010100: self.jump(arg1),
+    #             0b01010110: self.jne(arg1),
+    #             0b10000010: self.ldi(arg1, arg2),
+    #             0b10100010: self.mul_alu(arg1, arg2),
+    #             0b01000110: self.pop(arg1),
+    #             0b01001000: self.pra(arg1),
+    #             0b01000111: self.prn(arg1),
+    #             0b01000101: self.push(arg1),
+    #             0b00010001: self.ret_sub(),
+    #             0b10000100: "ST"
+    #         }
+            
+    #         ops[IR]
